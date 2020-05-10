516d9d9ea691fd1298b160ca9f1ce7d7e3d12f23
unknown <vincentjabonete10@gmail.com>
2020-05-10 20:16:05 +0800

    Initial commit

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ app.use(express.static(__dirname + '/dist'));
 // Heroku port
 app.listen(process.env.PORT || 8080);
 
-app.get('/*', function(res, req){
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
-})
+app.get('/',function(req, res){
+    res.sendFile(path.join(__dirname+'/index.html'));
+  });
 
 console.log('Console Listening!');
