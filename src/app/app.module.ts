94aaa1d443d70f33684e04b6c2b57265d8ba119b
unknown <vincentjabonete10@gmail.com>
2020-05-10 16:11:36 +0800

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {
+        path:'home',
+        component: HomeComponent
+      }
+
+    ])
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
