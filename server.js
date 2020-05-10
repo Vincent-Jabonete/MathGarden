695e50c9b4107279490628e3d220df97a2f950c0
unknown <vincentjabonete10@gmail.com>
2020-05-10 21:17:08 +0800

    make it better

--- server.js
+++ server.js
@@ -1,22 +1,29 @@
+// server.js
 const express = require('express');
 const app = express();
-const path = require('path');
-const port = process.env.PORT || 8000;
-const server = require('http').Server(app);
+const path = require ('path');
+// Run the app by serving the static files
+// in the dist directory
+app.use(express.static(__dirname + '/dist'));
+// Start the app by listening on the default
+// Heroku port
+app.listen(process.env.PORT || 8080);
 
-app.use(express.static(__dirname, 'dist', {index: false}));
+// app.get('/',function(req, res){
+//     res.sendFile(path.join(__dirname+'/index.html'));
+//   });
 
+  app.get('/*', function(req,res) {
+    
+    res.sendFile(path.join(__dirname +'/dist/mathgarden/index.html'));
+    });
 
-server.listen(port, function() {
-    console.log("App running on port " + port);
-})
+    app.get('', function(req, res) {
+        res.sendFile(path.join(__dirname +'/dist/mathgarden/index.html'));
+    });
+    
+    app.get('/', function(req, res) {
+        res.sendFile(path.join(__dirname +'/dist/mathgarden/index.html'));
+    });
 
-// PathLocationStrategy
-
-app.get('', function(req, res) {
-    res.sendFile(path.join(__dirname, 'src', 'index.html'));
-});
-
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, 'src', 'index.html'));
-});
+console.log('Console Listening!');
