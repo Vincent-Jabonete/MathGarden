250ac63caa411de146b141281314ef6d176ceb5f
unknown <vincentjabonete10@gmail.com>
2020-05-10 21:59:36 +0800

    make it better

--- server.js
+++ server.js
@@ -1,17 +1,21 @@
+// server.js
 const express = require('express');
 const app = express();
-const path = require('path');
-const port = process.env.PORT || 8000;
-const server = require('http').Server(app);
-
+const path = require ('path');
+// Run the app by serving the static files
+// in the dist directory
 app.use(express.static(__dirname + '/src'));
+// Start the app by listening on the default
+// Heroku port
+app.listen(process.env.PORT || 8080);
 
-server.listen(port, function() {
-    console.log("App running on port " + port);
-})
+// app.get('/',function(req, res){
+//     res.sendFile(path.join(__dirname+'/index.html'));
+//   });
 
-// PathLocationStrategy
+  app.get('/*', function(req,res) {
+    
+    res.sendFile(path.join(__dirname+'/src/app/home/home.component.html'));
+    });
 
-app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/src/index.html'));
-})
+console.log('Console Listening!');
