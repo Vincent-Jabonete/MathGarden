889eb4d81c59010e5ec9c34db6076f59c7069104
unknown <vincentjabonete10@gmail.com>
2020-05-11 22:13:25 +0800

    sana gumana na bootstrap

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
-
+import * as jQuery from 'jquery';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
