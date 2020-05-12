04dfeb9fb5ab00a52f3c1b86cc8f535a73c1b765
unknown <vincentjabonete10@gmail.com>
2020-05-12 10:45:21 +0800

    style

--- server.js
+++ server.js
@@ -1,20 +1,19 @@
-
-const path = require('path');
+// server.js
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 8080;
+const path = require ('path');
 
-
-
-// Run the app by serving the static files in the dist directory
+// in the dist directory
 app.use(express.static(__dirname + '/dist'));
 
-// Start the app by listening on the default Heroku port
-app.listen(port);
 
-// For all GET requests, send back index.html so that PathLocationStrategy can be used
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+// Start the app by listening on the default
+// Heroku port
+
+
+app.listen(process.env.PORT || 8080);
+
+app.get('/',function(req, res){
+  res.sendFile(path.join(__dirname+'/index.html'));
 });
 
-console.log(`Server listening on ${port}`);
