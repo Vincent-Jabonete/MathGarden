fecb3a67669a70694dbf52adff283ea1d72e801c
unknown <vincentjabonete10@gmail.com>
2020-05-12 10:01:55 +0800

    Changes to style

--- server.js
+++ server.js
@@ -1,24 +1,21 @@
-// server.js
+const compression = require('compression');
+const path = require('path');
 const express = require('express');
 const app = express();
-const path = require ('path');
-// Run the app by serving the static files
-// in the dist directory
-app.use(express.static(__dirname + '/src'));
-// Start the app by listening on the default
-// Heroku port
-app.listen(process.env.PORT || 8080);
+const port = process.env.PORT || 8080;
 
-// app.get('/',function(req, res){
-//     res.sendFile(path.join(__dirname+'/index.html'));
-//   });
+// Gzip
+app.use(compression());
 
-  app.get('/*', function(req,res) {
-    
-    res.sendFile(path.join(__dirname+'/src/app/home/home.component.html'));
-    res.sendFile(path.join(__dirname+'/src/app/home/home.component.css'));
-    res.sendFile(path.join(__dirname+'/src/app/home/home.component.ts'));
-    res.sendFile(path.join(__dirname+'/src/app/home/home.component.spec.ts'));
-  });
+// Run the app by serving the static files in the dist directory
+app.use(express.static(__dirname + '/dist'));
 
-console.log('Console Listening!');
+// Start the app by listening on the default Heroku port
+app.listen(port);
+
+// For all GET requests, send back index.html so that PathLocationStrategy can be used
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
+
+console.log(`Server listening on ${port}`);
