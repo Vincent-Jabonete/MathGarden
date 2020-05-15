4d73265de5ab7e667a53b5e1525c22a3b80cd45e
unknown <vincentjabonete10@gmail.com>
2020-05-15 13:03:34 +0800

    done services and recent design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,10 +10,22 @@ import { RouterModule } from '@angular/router';
 import {NgbPaginationModule, NgbAlertModule} from '@ng-bootstrap/ng-bootstrap';
 import {MatButtonModule} from '@angular/material/button';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { AboutComponent } from './about/about.component';
+import {ServicesComponent} from './services/services.component';
+import { ContactComponent } from './contact/contact.component';
+import { ModalLogoutComponent } from './modal-logout/modal-logout.component';
+import { MatDialogModule } from '@angular/material/dialog'
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
+// import { Ng2CarouselamosModule } from 'ng2-carouselamos';
+
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    AboutComponent,
+    ServicesComponent,
+    ContactComponent,
+    ModalLogoutComponent,
   ],
   imports: [
     BrowserModule,
@@ -22,11 +34,27 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
     NgbAlertModule,
     NgbModule,
     MatButtonModule,
+    MatDialogModule ,
+    BrowserAnimationsModule,
+    // Ng2CarouselamosModule,
+
  
     RouterModule.forRoot([
       {
         path:'home',
         component: HomeComponent
+      },
+      {
+        path:'about',
+        component: AboutComponent
+      },
+      {
+        path:'services',
+        component:ServicesComponent
+      },
+      {
+        path:'contact',
+        component:ContactComponent
       }
 
     ])
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {MatButtonModule} from '@angular/material/button';
-import * as jQuery from 'jquery';
+
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -10,11 +10,15 @@ export class HomeComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
 
 
 
+
   images = ['../../assets/img/1.png', '../../assets/img/7.png','../../assets/img/9.png'];
+
+
+  
 
 }
