8ec840a08b930c6b9375afb8ac02533ce466eb81
unknown <vincentjabonete10@gmail.com>
2020-05-16 10:37:32 +0800

    updated carousel column

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -14,45 +14,7 @@ export class HomeComponent implements OnInit {
   }
 
   images = ['../../assets/img/1.png', '../../assets/img/7.png','../../assets/img/9.png'];
-  customOptions: any = {
-    loop: true,
-    mouseDrag: false,
-    touchDrag: false,
-    pullDrag: false,
-    dots: false,
-    navSpeed: 700,
-    navText: ['', ''],
-    responsive: {
-      0: {
-        items: 1
-      },
-      400: {
-        items: 2
-      },
-      740: {
-        items: 3
-      },
-      940: {
-        items: 4
-      }
-    },
-    nav: true
-  }
 
-read(){
-  
-  var more = document.getElementById("more");
-  var read = document.getElementById("read");
-  if(more.style.display == 'block') {
-    more.style.display = 'none';
-    read.innerHTML = "Read more";
-    
-  } else {
-    more.style.display = 'block';
-    read.innerHTML = "Read less";
-  }
-
-}
   
 
 }
