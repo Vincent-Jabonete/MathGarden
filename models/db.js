07b385f89690b1292345591aa2be8cd57811c1ee
unknown <vincentjabonete10@gmail.com>
2020-05-28 08:15:58 +0800

    update db

--- models/db.js
+++ models/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI,
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://Vincent:Jabonete@mathgarden-yqteo.mongodb.net/test?retryWrites=true&w=majority',
     {
         useNewUrlParser: true,
         useUnifiedTopology: true,
